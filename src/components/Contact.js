647d4c1811b5dcbbd0a5b779194d9e5fb1f5e8b4
Abhishek <shindeaa21.comp@coep.ac.in>
2022-10-02 12:46:22 +0530

    added 'msg sent' toast

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,9 @@ import emailjs from '@emailjs/browser';
 import callIcon from './img/icons8-phone-96.png';
 import mailIcon from './img/icons8-mail-96.png';
 import linkedIcon from './img/icons8-linkedin-96.png';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
 
 export default function Contact() {
     const form = useRef();
@@ -17,43 +20,59 @@ export default function Contact() {
             }, (error) => {
                 console.log(error.text);
             });
+
+
+        setTimeout(() => { e.target.reset(); }, "2000")
     };
+
+    const notify = () => {
+        toast("Message sent successfully!!");
+
+    }
+
+
+    const [Name, onChangeName] = useState("");
+    const [Email, onChangeEmail] = useState("");
+    const [Msg, onChangeMsg] = useState("");
+
+    const areAllFieldsFilled = (Name !== "") && (Email !== "") && (Msg !== "");
 
     return (
         <div id="contact">
+            <ToastContainer />
             <section className="contactSection section" id="sect">
                 <h1 className="contactTitle">Contact Me</h1>
-                    <div className="contactInfo1">
-                        <div className="mob">
-                            <img src={callIcon} alt="" />
-                            <h1>Call Me on</h1>
-                            <h3>+91 7057870097</h3>
-                        </div>
-                        <div className="mail">
-                            <img src={mailIcon} alt="" />
-                            <h1>Mail me at</h1>
-                            <h3>abhishinde889@gmail.com</h3>
-                        </div>
-                        <div className="linkedin">
-                            <img src={linkedIcon} alt="" />
-                            <h1>LinkedIn</h1>
-                            <a href="https://www.linkedin.com/in/abhishek-shinde-7bb403230/">
-                                <h3>https://www.linkedin.com/in/ <br /> abhishek-shinde-7bb403230/</h3>
-                            </a>
-                        </div>
+                <div className="contactInfo1">
+                    <div className="mob">
+                        <img src={callIcon} alt="" />
+                        <h1>Call Me on</h1>
+                        <h3>+91 7057870097</h3>
                     </div>
-                    <div className="contactInfo2">
-                    <h1 className="contactTitle2">Message Me</h1>
-                        <form ref={form} onSubmit={sendEmail}>
-                            <label>Your Name</label>
-                            <input type="text" name="user_name" placeholder="Enter Your Name" />
-                            <label>Your Email</label>
-                            <input type="email" name="user_email" placeholder="Enter Your Mail Id" />
-                            <label>Message</label>
-                            <textarea name="message" placeholder="Say Something..." />
-                            <input type="submit" value="Send" />
-                        </form>
+                    <div className="mail">
+                        <img src={mailIcon} alt="" />
+                        <h1>Mail me at</h1>
+                        <h3>abhishinde889@gmail.com</h3>
                     </div>
+                    <div className="linkedin">
+                        <img src={linkedIcon} alt="" />
+                        <h1>LinkedIn</h1>
+                        <a href="https://www.linkedin.com/in/abhishek-shinde-7bb403230/">
+                            <h3>https://www.linkedin.com/in/ <br /> abhishek-shinde-7bb403230/</h3>
+                        </a>
+                    </div>
+                </div>
+                <div className="contactInfo2">
+                    <h1 className="contactTitle2">Feel Free To Message Me</h1>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <label>Your Name</label>
+                        <input type="text" name="user_name" placeholder="Tell Me Your Name" autoComplete="off" onChange={ e => onChangeName(e.target.value)} required />
+                        <label>Your Email</label>
+                        <input type="email" name="user_email" placeholder="Your Mail Id" onChange={ e => onChangeEmail(e.target.value)} required />
+                        <label>Message</label>
+                        <textarea name="message" placeholder="Say Something..." rows="5" autoComplete="off" onChange={ e => onChangeMsg(e.target.value)} required />
+                        <input type="submit" value="Send" onClick={notify} disabled={!areAllFieldsFilled} />
+                    </form>
+                </div>
             </section>
         </div>
     )
